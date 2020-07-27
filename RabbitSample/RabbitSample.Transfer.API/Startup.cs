a9e45fd7f533b2be17dc902fefb1b0748f3b5407
Gracjan <graku12@gmail.com>
2020-07-27 20:58:09 +0200

    Added Transfer project

--- RabbitSample/RabbitSample.Transfer.API/Startup.cs
+++ RabbitSample/RabbitSample.Transfer.API/Startup.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using RabbitSample.Infrastructure.IoC;
+using RabbitSample.Transfer.Data.Context;
+using RabbitSimple.Banking.Data.Context;
 
 namespace RabbitSample.Transfer.API
 {
@@ -25,7 +32,24 @@ namespace RabbitSample.Transfer.API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddControllers();
+      services.AddDbContext<TransferDbContext>(options =>
+      {
+        options.UseSqlServer(Configuration.GetConnectionString("TransferDbConnectionString"));
+      });
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Transfer Microservice", Version = "v1" });
+      });
+
+      services.AddControllers().AddJsonOptions(options =>
+      {
+        options.JsonSerializerOptions.WriteIndented = true;
+        options.JsonSerializerOptions.Encoder = JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All);
+      });
+
+      services.RegisterServices();
+      services.AddMediatR(typeof(Startup));
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +59,12 @@ namespace RabbitSample.Transfer.API
       {
         app.UseDeveloperExceptionPage();
       }
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Transfer.API");
+      });
 
       app.UseHttpsRedirection();
 
---
+++ RabbitSample/RabbitSample.Transfer.Data/Context/TransferDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RabbitSample.Transfer.Domain.Models;
+
+namespace RabbitSample.Transfer.Data.Context
+{
+  public class TransferDbContext : DbContext
+  {
+    public TransferDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<TransferLog> TransferLogs { get; set; }
+  }
+}
